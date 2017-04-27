8c1286f35ab212c3025e3f30226ca22e4376bb6f
Bright <2412944911@qq.com>
2017-04-27 16:58:06 +0800

    12

--- src/components/edit-form.vue
+++ src/components/edit-form.vue
@@ -6,8 +6,8 @@
       </el-form-item>
       <el-form-item label="活动区域">
         <el-select v-model="form.region" placeholder="请选择活动区域">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
+          <el-option label="区域一1" value="shanghai"></el-option>
+          <el-option label="区域二2" value="beijing"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="活动时间">
